c3b2e19e51377fed26c63bcecdb7af88cc7589aa
Diachenko Maksym <52499153+maksymkaD@users.noreply.github.com>
2022-11-07 21:28:49 +0200

    Save Messages to MongoDB
    Added mvn library, allowing to save the messages to the database

--- kmaooad-functions/src/main/java/edu/kmaooad/Function.java
+++ kmaooad-functions/src/main/java/edu/kmaooad/Function.java
@@ -1,5 +1,6 @@
 package edu.kmaooad;
 
+import com.google.gson.Gson;
 import com.microsoft.azure.functions.ExecutionContext;
 import com.microsoft.azure.functions.HttpMethod;
 import com.microsoft.azure.functions.HttpRequestMessage;
@@ -8,9 +9,17 @@ import com.microsoft.azure.functions.HttpStatus;
 import com.microsoft.azure.functions.annotation.AuthorizationLevel;
 import com.microsoft.azure.functions.annotation.FunctionName;
 import com.microsoft.azure.functions.annotation.HttpTrigger;
+import com.mongodb.ConnectionString;
+import com.mongodb.client.MongoClient;
+import com.mongodb.MongoClientSettings;
+import com.mongodb.client.MongoClients;
+import com.mongodb.client.MongoCollection;
+import com.mongodb.client.MongoDatabase;
+import org.bson.Document;
 import org.json.*;
 
 
+import java.util.HashMap;
 import java.util.Optional;
 
 /**
@@ -24,18 +33,29 @@ public class Function {
     @FunctionName("TelegramWebhook")
     public HttpResponseMessage run(
             @HttpTrigger(
-                name = "req",
-                methods = {HttpMethod.POST},
-                authLevel = AuthorizationLevel.FUNCTION)
-                HttpRequestMessage<Optional<String>> request,
+                    name = "req",
+                    methods = {HttpMethod.POST},
+                    authLevel = AuthorizationLevel.FUNCTION)
+                    HttpRequestMessage<Optional<String>> request,
             final ExecutionContext context) {
         context.getLogger().info("Java HTTP trigger processed a request.");
+
 
         final String bodyJson = request.getBody().orElse(null);
         Long messageId;
         try {
+            ConnectionString connectionString = new ConnectionString("mongodb+srv://admin:0FuzYyhgLsJU6nS9@cluster0.1tup9.mongodb.net/?retryWrites=true&w=majority");
+            MongoClientSettings settings = MongoClientSettings.builder()
+                    .applyConnectionString(connectionString)
+                    .build();
+            MongoClient mongoClient = MongoClients.create(settings);
+            MongoDatabase database = mongoClient.getDatabase("test");
+            MongoCollection<Document> collection = database.getCollection("requests");
+
             JSONObject obj = new JSONObject(bodyJson);
             messageId = obj.getJSONObject("message").getLong("message_id");
+            HashMap<String, Object> yourHashMap = new Gson().fromJson(obj.toString(), HashMap.class);
+            collection.insertOne(new Document(yourHashMap));
         } catch (JSONException jse) {
             return request.createResponseBuilder(HttpStatus.OK).body("Body without JSON").build();
         } catch (NullPointerException npe) {
